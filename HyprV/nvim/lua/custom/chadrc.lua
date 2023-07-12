0012118cdee26401809db0b354b3a49bd251a3c5
archimickey <wuchunyat119@gmail.com>
2023-07-12 14:37:40 +0800

    format

--- HyprV/nvim/lua/custom/chadrc.lua
+++ HyprV/nvim/lua/custom/chadrc.lua
@@ -4,30 +4,31 @@
     theme = 'catppuccin',
 
     hl_add = {
-      Venv = {
-        bold = true,
-        fg = "#b7bdf8"
-      }
-
+        Venv = {
+            bold = true,
+            fg = "#b7bdf8"
+        }
     },
     statusline = {
-      overriden_modules = function()
-        local st_modules = require "nvchad_ui.statusline.default"
-        local current_venv = require('swenv.api').get_current_venv()
-        local name
+        theme = 'default',
+        separator_style = 'default',
+        overriden_modules = function()
+          local st_modules = require "nvchad_ui.statusline.default"
+          local current_venv = require('swenv.api').get_current_venv()
+          local name
 
-        if current_venv ~= nil
-          then name = string.format(" %s ", current_venv.name)
-        else
-          name = " base "
-        end
+          if current_venv ~= nil
+            then name = string.format(" %s ", current_venv.name)
+          else
+            name = " base "
+          end
 
-        return {
-          mode = function()
-            return st_modules.mode() .. "%#Venv#" .. name
-          end,
-        }
-      end,
+          return {
+            mode = function()
+              return st_modules.mode() .. "%#Venv#" .. name
+            end,
+          }
+        end,
     },
 
     nvdash = {
