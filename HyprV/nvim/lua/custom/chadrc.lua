5a786c939abe2b0e08408f10e610d424a8d0742f
archimickey <wuchunyat119@gmail.com>
2023-07-12 12:40:21 +0800

    add transparency

--- HyprV/nvim/lua/custom/chadrc.lua
+++ HyprV/nvim/lua/custom/chadrc.lua
@@ -1,35 +1,40 @@
 ---@type ChadrcConfig 
  local M = {}
  M.ui = {
-  theme = 'catppuccin',
-  hl_add = {
-    Venv = {
-      bold = true,
-      fg = "#b7bdf8"
-    }
-  },
-  statusline = {
-    overriden_modules = function()
-      local st_modules = require "nvchad_ui.statusline.default"
-      local current_venv = require('swenv.api').get_current_venv()
-      local name
+    theme = 'catppuccin',
 
-      if current_venv ~= nil
-        then name = string.format(" %s ", current_venv.name)
-      else
-        name = " base "
-      end
-
-      return {
-        mode = function()
-          return st_modules.mode() .. "%#Venv#" .. name
-        end,
+    hl_add = {
+      Venv = {
+        bold = true,
+        fg = "#b7bdf8"
       }
-    end,
-  },
+
+    },
+    statusline = {
+      overriden_modules = function()
+        local st_modules = require "nvchad_ui.statusline.default"
+        local current_venv = require('swenv.api').get_current_venv()
+        local name
+
+        if current_venv ~= nil
+          then name = string.format(" %s ", current_venv.name)
+        else
+          name = " base "
+        end
+
+        return {
+          mode = function()
+            return st_modules.mode() .. "%#Venv#" .. name
+          end,
+        }
+      end,
+    },
+
     nvdash = {
         load_on_startup = true
-    }
+    },
+
+    transparency = true,
  }
  M.plugins = "custom.plugins"
  M.mappings = require "custom.mappings"
